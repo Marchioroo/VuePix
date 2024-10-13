06d4ad68e73c097ac17812f8edf2560ce1a1379d
Bruna Costa <brunadacosta2107@gmail.com>
2024-10-13 20:34:06 -0300

    inicial

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,20 @@
 <template>
+
+
   <div class="container mx-auto p-4 ">
-    
-    <button @click="openModal" class="mt-4 bg-green-500 text-white p-2 rounded">Pix e saques</button>
+    <div class="w-full h-0.5 w-full  bg-gray-200 justify-start w-full max- md:w-1/3 lg:w-[30%]" ></div>
+    <div class=" mt-3 flex items-center justify-start w-full md:w-1/3 lg:w-[30%]">
+    <img src="../src/assets/ic/ic-pix.svg" class="mr-2 w-6 h-6">
+  
+    <button @click="openModal" class=" text-black  rounded"> Pix e saques</button>
+    <img src="../src/assets/ic/ic-Arrows-Tipe_2-Direction_Right.svg" class="mr-2 ml-auto w-6 h-4">
     <ModalPage :isOpen="isModalOpen" @close="closeModal" />
   </div>
+</div>
+
+
+
+
 </template>
 
 <script>
@@ -33,4 +44,5 @@ export default {
 </script>
 
 <style>
+    @import url('https://fonts.googleapis.com/css2?family=Manrope:wght@200..800&display=swap'),
 </style>
